4c7bc29eda9792c7592ff3075f1bd21820b56c2c
martezr <martez.reed@greenreedtech.com>
2017-03-01 08:56:42 -0600

    Update jenkinssettings API

--- sinatra/data/app.rb
+++ sinatra/data/app.rb
@@ -1,13 +1,19 @@
 require 'sinatra'
 require 'json'
 require 'etcd'
-
+require 'bunny'
 
 set :bind, '0.0.0.0'
 
 
 set :public_folder, 'public'
 set :views, 'views'
+
+@rabbitmq_server = 'rabbitmq'
+@etcd_server = 'etcd'
+@etcd_port = '4001'
+
+
 
 get '/' do
   erb :home
@@ -18,7 +24,25 @@ get '/configuration' do
   erb :configure
 end
 
-post '/configuration' do
+post '/jenkinssettings' do
+  client = Etcd.client(host: @etcd_server, port: @etcd_port)
+  client.set('/configuration/jenkins_url', value: "#{params['jenkins_url']}")
+  client.set('/configuration/jenkins_username', value: "#{params['jenkins_username']}")
+  client.set('/configuration/jenkins_password', value: "#{params['jenkins_password']}")
+  client.set('/configuration/jenkins_sshkey', value: "#{params['jenkins_sshkey']}")
+
+
+  conn = Bunny.new(:hostname => @rabbitmq_server)
+  conn.start
+
+  ch   = conn.create_channel
+  q    = ch.queue("jjb")
+  ch.default_exchange.publish("Update Jenkins", :routing_key => jenkinssettings)
+  puts " [x] Sent 'Update Jenkins Settings'"
+  conn.close
+end
+
+post '/puppetsettings' do
   params.to_s
   tempHash = {
     "Server" => "#{params['controlrepo']}",
@@ -26,14 +50,6 @@ post '/configuration' do
   File.open("public/spells.json","w") do |f|
     f.write(tempHash.to_json)
   end
-  client = Etcd.client(host: '192.168.1.229', port: 4001)
-  client.set('/configuration/controlrepo', value: "#{params['controlrepo']}")
-  client.set('/configuration/jenkins_url', value: "#{params['jenkins_url']}")
-  client.set('/configuration/jenkins_username', value: "#{params['jenkins_username']}")
-  client.set('/configuration/jenkins_password', value: "#{params['jenkins_password']}")
-
-  status, headers, body = call env.merge("PATH_INFO" => '/update')
-  [status, headers, body.map(&:upcase)]
 end
 
 post '/update' do
