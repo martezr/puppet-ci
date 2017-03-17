82f2965758896603db9e3bc7fc3fbd70331f61ac
martezr <martez.reed@greenreedtech.com>
2017-03-17 16:47:57 -0500

    Add jjb logic to create Jenkins puppet module jobs

--- jjb/data/jjb_receive.rb
+++ jjb/data/jjb_receive.rb
@@ -126,6 +126,46 @@ def updatejenkinssettings
   updateconfig()
 end
 
+def addpuppetmodule
+  uri = URI.parse("http://etcd:2379/v2/keys/configuration/modules")
+  response = Net::HTTP.get_response(uri)
+
+  result = response.body
+
+  parsed = JSON.parse(result)
+  modules = parsed['node']['nodes']
+
+  modulejobs = File.open("./modules/puppetmodules.yaml", "w")
+  modulejobs.puts "- project:"
+  modulejobs.puts "    name: puppet-modules"
+  modulejobs.puts "    jobs:"
+
+  for puppetmodule in modules
+    modulepath = puppetmodule['key']
+    uribase = "http://etcd:2379/v2/keys" + modulepath
+    uri = URI.parse("#{uribase}")
+    response = Net::HTTP.get_response(uri)
+    result = response.body
+    parsed = JSON.parse(result)
+    keys = parsed['node']['nodes']
+    for key in keys
+      teststring = key['key']
+      if teststring.include? "name"
+        @name = key['value']
+      elsif teststring.include? "branch"
+        @branch = key['value']
+      elsif teststring.include? "url"
+        @url = key['value']
+      end
+    end
+    modulejobs.puts '      - puppet-module-{name}:'
+    modulejobs.puts "         name: #@name"
+    modulejobs.puts "         url: #@url"
+    modulejobs.puts "         branch: #@branch"      
+  end
+  modulejobs.close
+end
+
 begin
   retries ||= 0
   puts 'sleeping for 15 seconds while rabbitmq boots'
@@ -143,11 +183,20 @@ q    = ch.queue("jjb")
 puts " [*] Waiting for messages in #{q.name}."
 q.subscribe(:manual_ack => true, :block => true) do |delivery_info, properties, body|
   `echo "[x] Received #{body}" >> /var/log/jjb.log` 
-  updatejob()
-  updatejenkinssettings()
-  adddockerjenkinscreds()
-  addjenkinssshcredentials()
-  `jenkins-jobs --conf jenkins_job.ini update controlrepo.yaml`
+  
+  # Conditional logic to determine action taken
+  case body
+  when "Update Jenkins"
+    updatejenkinssettings()
+    adddockerjenkinscreds()
+    addjenkinssshcredentials()
+  when "Update Puppet"
+    updatejob()
+    `jenkins-jobs --conf jenkins_job.ini update controlrepo.yaml`
+  when "Add Puppet Module"
+    addpuppetmodule()
+    `jenkins-jobs --conf jenkins_job.ini update ./modules/`
+  end    
 
   ch.ack(delivery_info.delivery_tag)
 end
