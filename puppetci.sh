c201ed6c5a5290449055e251c598b6f008c61cc8
Martez Reed <martez.reed@kinneygroup.com>
2017-04-05 14:51:25 -0500

    Cleanup extraneous code

--- puppetci.sh
+++ puppetci.sh
@@ -11,12 +11,12 @@ cp -R ansible/ /etc/
 setenforce 0
 
 # Install Docker
-ansible-playbook /etc/ansible/install.yml
+time ansible-playbook /etc/ansible/install.yml
 
 # Set docker host 
 CONTAINER_HOST=$(ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+')
 sed -i "s#tcp://:2375#tcp://$CONTAINER_HOST:2375#" jenkins/config.xml
 
-docker-compose up -d
+time docker-compose up -d
 cd docker-jenkins-slave
-sh build.sh
+time sh build.sh
